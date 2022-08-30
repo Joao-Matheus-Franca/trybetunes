8a9a52b84e558a86b5637b010c4cb9811a7dca74
João Matheus Silva Franca <matheussfranca@live.com>
2022-08-30 16:28:07 -0300

    Concluído: Requisito 02

--- src/App.js
+++ src/App.js
@@ -11,17 +11,20 @@ import NotFound from './pages/NotFound';
 class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={ Login } />
-          <Route path="/search" component={ Search } />
-          <Route path="/album/:id" component={ Album } />
-          <Route path="/favorites" component={ Favorites } />
-          <Route exact path="/profile" component={ Profile } />
-          <Route path="/profile/edit" component={ ProfileEdit } />
-          <Route exact path="*" component={ NotFound } />
-        </Switch>
-      </BrowserRouter>
+      <>
+        <h1>TybeTune</h1>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={ Login } />
+            <Route path="/search" component={ Search } />
+            <Route path="/album/:id" component={ Album } />
+            <Route path="/favorites" component={ Favorites } />
+            <Route exact path="/profile" component={ Profile } />
+            <Route path="/profile/edit" component={ ProfileEdit } />
+            <Route exact path="*" component={ NotFound } />
+          </Switch>
+        </BrowserRouter>
+      </>
     );
   }
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,64 @@
 import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Login extends React.Component {
+  state = {
+    name: '',
+    btnDisabled: true,
+    loading: false,
+    redirect: false,
+  };
+
+  handleChange = (event) => {
+    const { target: { value } } = event;
+    this.setState({ name: value }, () => this.activateBtn());
+  };
+
+  activateBtn = () => {
+    const { name } = this.state;
+    const minLength = 3;
+    if (name.length >= minLength) {
+      this.setState({ btnDisabled: false });
+    } else {
+      this.setState({ btnDisabled: true });
+    }
+  };
+
+  handleClick = async () => {
+    const { name } = this.state;
+    this.setState({ loading: true });
+    await createUser({ name });
+    this.setState({ redirect: true });
+  };
+
   render() {
+    const { btnDisabled, loading, redirect } = this.state;
     return (
-      <div data-testid="page-login">
-        <h1>Login</h1>
-      </div>
+      <Route exact path="/">
+        {loading ? <Loading /> : (
+          <div data-testid="page-login">
+            <h1>Login</h1>
+            <label htmlFor="loginInput">
+              <input
+                data-testid="login-name-input"
+                type="text"
+                onChange={ this.handleChange }
+              />
+            </label>
+            <button
+              type="button"
+              data-testid="login-submit-button"
+              disabled={ btnDisabled }
+              onClick={ this.handleClick }
+            >
+              Entrar
+            </button>
+          </div>
+        )}
+        {redirect && <Redirect to="/search" />}
+      </Route>
     );
   }
 }
