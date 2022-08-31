ab8ea933f5edf7390696f798ee475caf15d5dce7
João Matheus Silva Franca <matheussfranca@live.com>
2022-08-31 10:43:17 -0300

    Concluído: Requisito 10

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import MusicCard from '../components/MusicCard';
 import getMusics from '../services/musicsAPI';
-import { addSong } from '../services/favoriteSongsAPI';
+import { addSong, getFavoriteSongs } from '../services/favoriteSongsAPI';
 import Loading from './Loading';
 
 class Album extends React.Component {
@@ -13,6 +13,7 @@ class Album extends React.Component {
     musics: [],
     loading: false,
     favorites: [],
+    getFavorites: [],
   };
 
   async componentDidMount() {
@@ -23,7 +24,11 @@ class Album extends React.Component {
       artist: infos.artistName,
       album: infos.collectionName,
       musics });
-    console.log(musics);
+
+    this.setState({ loading: true });
+    const favorites = await getFavoriteSongs();
+    const songs = favorites.map((song) => song[0]);
+    this.setState({ loading: false, getFavorites: songs });
   }
 
   getId = () => {
@@ -46,7 +51,7 @@ class Album extends React.Component {
   };
 
   render() {
-    const { artist, album, musics, loading, favorites } = this.state;
+    const { artist, album, musics, loading, favorites, getFavorites } = this.state;
     return (
       <>
         <Header />
@@ -62,7 +67,10 @@ class Album extends React.Component {
                 previewUrl={ music.previewUrl }
                 trackId={ music.trackId }
                 onChange={ this.handleChange }
-                checked={ favorites.includes(music.trackId.toString()) }
+                checked={ (getFavorites[0] !== undefined)
+                  ? (getFavorites.some((e) => e.trackId === music.trackId)
+                  || favorites.includes(music.trackId.toString()))
+                  : favorites.includes(music.trackId.toString()) }
               />))}
           </div>)}
       </>
--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
+import MusicCard from '../components/MusicCard';
+import Loading from './Loading';
 
 class Favorites extends React.Component {
+  state = {
+    favorites: [],
+    loading: false,
+    checked: true,
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const musics = await getFavoriteSongs();
+    this.setState({ loading: false, favorites: musics });
+  }
+
   render() {
+    const { favorites, loading, checked } = this.state;
     return (
       <div data-testid="page-favorites">
         <h1>Favorites</h1>
+        {loading ? <Loading /> : favorites.map((music) => (
+          <MusicCard
+            key={ music.trackName }
+            trackName={ music.trackName }
+            previewUrl={ music.previewUrl }
+            trackId={ music.trackId }
+            checked={ checked }
+          />))}
+
       </div>
     );
   }
