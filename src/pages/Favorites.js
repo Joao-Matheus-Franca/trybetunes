9704522d69a904232495616206208c8169746d0f
João Matheus Silva Franca <matheussfranca@live.com>
2022-09-04 18:18:32 -0300

    Concluído: Requisito 12

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { getFavoriteSongs } from '../services/favoriteSongsAPI';
+import { getFavoriteSongs, removeSong } from '../services/favoriteSongsAPI';
 import MusicCard from '../components/MusicCard';
 import Loading from './Loading';
 
@@ -7,7 +7,6 @@ class Favorites extends React.Component {
   state = {
     favorites: [],
     loading: false,
-    checked: true,
   };
 
   async componentDidMount() {
@@ -16,8 +15,18 @@ class Favorites extends React.Component {
     this.setState({ loading: false, favorites: musics });
   }
 
+  handleChange = async (event) => {
+    const { target: { id } } = event;
+    const { favorites } = this.state;
+    const song = favorites.find((s) => s.trackId.toString() === id);
+    this.setState({ loading: true });
+    await removeSong(song);
+    const musics = await getFavoriteSongs();
+    this.setState({ loading: false, favorites: musics });
+  };
+
   render() {
-    const { favorites, loading, checked } = this.state;
+    const { favorites, loading } = this.state;
     return (
       <div data-testid="page-favorites">
         <h1>Favorites</h1>
@@ -27,7 +36,8 @@ class Favorites extends React.Component {
             trackName={ music.trackName }
             previewUrl={ music.previewUrl }
             trackId={ music.trackId }
-            checked={ checked }
+            onChange={ this.handleChange }
+            checked={ favorites.some((song) => song.trackId === music.trackId) }
           />))}
 
       </div>
