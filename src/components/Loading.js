fd8af3f3f1dc547a02f1105ef7b699fbce33ed94
João Matheus Silva Franca <matheussfranca@live.com>
2023-07-28 14:38:37 -0300

    Finalizando estilização do componente musiccard

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Loading extends React.Component {
   render() {
     return (
-      <h2>Carregando...</h2>
+      <h2 className="loading">Carregando...</h2>
     );
   }
 }
--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -5,7 +5,7 @@ class MusicCard extends React.Component {
   render() {
     const { trackName, previewUrl, trackId, onChange, checked } = this.props;
     return (
-      <>
+      <div className="music-box">
         <p>{ trackName }</p>
         <audio src={ previewUrl } controls>
           <track kind="captions" />
@@ -15,18 +15,27 @@ class MusicCard extends React.Component {
           <code>audio</code>
           .
         </audio>
-        <label
-          htmlFor={ trackId }
-        >
-          Favorita
+        <label htmlFor={ trackId }>
           <input
+            className="favorite-checkbox"
             id={ trackId }
             type="checkbox"
             onChange={ onChange }
             checked={ checked }
           />
+          {
+            checked ? (<img
+              className="heart-icon"
+              src="https://www.svgrepo.com/show/525369/heart.svg"
+              alt="Ícone coração cheio"
+            />) : (<img
+              className="heart-icon"
+              src="https://www.svgrepo.com/show/532473/heart.svg"
+              alt="Ícone coração vazio"
+            />)
+          }
         </label>
-      </>
+      </div>
     );
   }
 }
