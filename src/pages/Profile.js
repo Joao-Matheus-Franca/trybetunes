3996dfea2d2d43a25c6c3951f31fef7f43d57056
João Matheus Silva Franca <matheussfranca@live.com>
2023-08-02 17:03:19 -0300

    Ajustes da estilização

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -22,25 +22,22 @@ class Profile extends React.Component {
 
   render() {
     const { loading, name, image, email, description } = this.state;
-    const profile = (
-      <div id="main-profile">
-        <h2 id="title-profile">Perfil</h2>
-        <ul id="profile-data-list">
-          <li id="profile-name">{name}</li>
-          <img
-            id="profile-image"
-            alt={ name }
-            src={ image || 'https://www.svgrepo.com/show/436841/person-crop-square-fill.svg' }
-          />
-          <li>{email}</li>
-          <li>{description}</li>
-          <Link id="edit-profile-button" to="/profile/edit">Editar perfil</Link>
-        </ul>
-      </div>);
     return (
-      <div data-testid="page-profile">
-        {loading ? <Loading /> : (profile)}
-      </div>
+      loading ? <Loading /> : (
+        <div id="main-profile">
+          <h2 id="title-profile">Perfil</h2>
+          <ul id="profile-data-list">
+            <li id="profile-name">{name}</li>
+            <img
+              id="profile-image"
+              alt={ name }
+              src={ image || 'https://www.svgrepo.com/show/436841/person-crop-square-fill.svg' }
+            />
+            <li>{email}</li>
+            <li>{description}</li>
+            <Link id="edit-profile-button" to="/profile/edit">Editar perfil</Link>
+          </ul>
+        </div>)
     );
   }
 }
